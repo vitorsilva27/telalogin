4e275799071bf8e381f7bdcd8509fabfcb29a663
VitorGabriel <vg285757@gmail.com>
2021-09-24 13:08:07 -0300

    Inicializando tela de login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const produtos = [
+  {
+    id: 1,
+    nome: 'Smartphone',
+    preco: 'R$ 2000',
+    cores: ['#29d8d5', '#252a34', '#fc3766'],
+  },
+  {
+    id: 2,
+    nome: 'Notebook',
+    preco: 'R$ 3000',
+    cores: ['#ffd045', '#d4394b', '#f37c59'],
+  },
+  {
+    id: 3,
+    nome: 'Tablet',
+    preco: 'R$ 1500',
+    cores: ['#365069', '#47c1c8', '#f95786'],
+  },
+];
+
+const App = () => {
+  return(
+     <section>
+       {produtos
+        .filter((produto) => Number(produto.preco.replace('R$', '')) > 1500)
+        .map((produto) => (
+          <div key={produto.id}>
+            <h1>{produto.nome}</h1>
+            <p>{produto.preco}</p>
+            <ul>{produto.cores.map((cor)=> (
+              <li key={cor} style={{backgroundColor: cor, color:'white'}}>
+                {cor}
+              </li>
+            ))}</ul>
+          </div>
+        ))}
+     </section>
+     
+     );
+};
 
 export default App;
